712abc736594544834ddefc30e842941a6e4dd0a
Yurii Zdyrko <yurii.zdyrko@prosapient.com>
2023-03-08 16:40:07 +0200

    basic LiveView "game"

--- lib/pento_web/live/wrong_live.ex
+++ lib/pento_web/live/wrong_live.ex
@@ -1,16 +1,16 @@
-#---
+# ---
 # Excerpted from "Programming Phoenix LiveView",
 # published by The Pragmatic Bookshelf.
 # Copyrights apply to this code. It may not be used to create training material,
 # courses, books, articles, and the like. Contact us if you are in doubt.
 # We make no guarantees that this code is fit for any purpose.
 # Visit https://pragprog.com/titles/liveview for more book information.
-#---
+# ---
 defmodule PentoWeb.WrongLive do
   use Phoenix.LiveView, layout: {PentoWeb.LayoutView, "live.html"}
 
   def mount(_params, _session, socket) do
-    {:ok, assign(socket, score: 0, message: "Make a guess:")}
+    {:ok, assign(socket, score: 0, message: "Make a guess:", time: time(), number: random())}
   end
 
   def render(assigns) do
@@ -24,19 +24,56 @@ defmodule PentoWeb.WrongLive do
         <a href="#" phx-click="guess" phx-value-number= {n} ><%= n %></a>
       <% end %>
     </h2>
+    <h2>
+      It's <%= @time %>
+    </h2>
+    <button phx-click="refreshtime">Refresh time</button>
     """
   end
 
-  def handle_event("guess", %{"number" => guess}=data, socket) do
-    message = "Your guess: #{guess}. Wrong. Guess again. "
-    score = socket.assigns.score - 1
+  def handle_event("guess", %{"number" => guess} = data, socket) do
+    number = socket.assigns.number
+    won = number == String.to_integer(guess)
+
+    message =
+      if won do
+        "Success! (#{guess})"
+      else
+        "Your guess: #{guess}. Wrong. Guess again. "
+      end
+
+    score =
+      if won do
+        socket.assigns.score + 1
+      else
+        socket.assigns.score - 1
+      end
 
     {
       :noreply,
       assign(
         socket,
         message: message,
-        score: score)}
+        score: score
+      )
+    }
   end
 
+  def handle_event("refreshtime", _, socket) do
+    {
+      :noreply,
+      assign(
+        socket,
+        time: time()
+      )
+    }
+  end
+
+  def time() do
+    DateTime.utc_now() |> to_string
+  end
+
+  def random() do
+    :rand.uniform(10)
+  end
 end
